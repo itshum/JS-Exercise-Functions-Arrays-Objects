239d51cb1332ccda196373a62ce727a8f8ef6c4a
Humayun Rashid <humayun@Humayuns-iMac.local>
2019-11-12 23:24:21 -0500

    finished exercise without stretch

--- index.js
+++ index.js
@@ -300,15 +300,20 @@ function getOlderCars(inventory, year) {
 */
 function getGermanCars(arr) {
   const cars = [];
+  // I need to look through the arr
+  // all of those car makes need to equal four brands, meet conditions, sounds like if/else
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      arr[i].car_make == "Audi" ||
+      arr[i].car_make == "Mercedes-Benz" ||
+      arr[i].car_make == "Volkswagen" ||
+      arr[i].car_make == "BMW" 
+    )
+      cars.push(arr[i]);
+    }
+  return cars;
 
-
-
-}
-
-
-
-
-
+  }
 
 
 
@@ -331,9 +336,26 @@ function getGermanCars(arr) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum = (a, b) => {
+  return a + b;
+};
+
+const addFive = num => {
+  return num + 5;
+};
+
+const argTimesTwo = num => {
+  return num * 2;
+};
+
+
+
+
+
+
+
+
+
 
 /**
  * ### Challenge `carMaker`
